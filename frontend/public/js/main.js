07f727e1b29efbc99826d04cb01055d4f92a1ff1
timo.waibel <timo.nwc@gmail.com>
2023-11-29 16:35:40 +0100

    frontend stuff

---
+++ frontend/public/js/main.js
@@ -0,0 +1,27 @@
+const searchBarInput = document.getElementById('search-bar-input');
+const searchForm = document.getElementById('search-form');
+const watersContainer = document.getElementById('waters-container');
+
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const searchQuery = searchBarInput.value;
+
+    watersContainer.innerHTML = '';
+
+    if (searchQuery === '') {
+        return;
+    }
+
+    const response = await fetch(`/api/waters/${searchQuery}`);
+    const waters = await response.json();
+
+    for (const water of waters) {
+        watersContainer.innerHTML += `
+            <div class="water">
+                <h2>${water.longname}</h2>
+                <h3>${water.shortname}</h3>
+            </div>
+        `;
+    }
+});
